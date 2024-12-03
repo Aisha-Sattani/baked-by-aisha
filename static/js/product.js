175e009b9f7efe0c929edc7c1aa79426c1965aea
basil-ahmed <bq2024@nyu.edu>
2024-12-02 18:06:37 -0600

    seperated cakes cupcakes

--- static/js/product.js
+++ static/js/product.js
@@ -18,6 +18,10 @@ const Product = () => {
     setModalProduct(null);
   };
 
+  // Filter products by category
+  const filterByCategory = (category) =>
+    products.filter((product) => product.category === category);
+
   return (
     <div className="container-xxl bg-light my-6 py-6 pt-0">
       <div className="container">
@@ -25,35 +29,110 @@ const Product = () => {
           <p className="text-primary text-uppercase mb-2">// Bakery Products</p>
           <h1 className="display-6 mb-4">Explore The Categories Of Our Bakery Products</h1>
         </div>
-        <div className="row g-4">
-          {products.map((product) => (
-            <div className="col-lg-4 col-md-6" key={product._id}>
-              <div className="product-item d-flex flex-column bg-white rounded overflow-hidden h-100">
-                <div className="text-center p-4">
-                  <div className="d-inline-block border border-primary rounded-pill px-3 mb-3">
-                    {product.price}
+
+        {/* Section for Cakes */}
+        <div className="mb-5">
+          <h2 className="text-primary mb-4">Cakes</h2>
+          <div className="row g-4">
+            {filterByCategory("cakes").map((product) => (
+              <div className="col-lg-4 col-md-6" key={product._id}>
+                <div className="product-item d-flex flex-column bg-white rounded overflow-hidden h-100">
+                  <div className="text-center p-4">
+                    <div className="d-inline-block border border-primary rounded-pill px-3 mb-3">
+                      {product.price}
+                    </div>
+                    <h3 className="mb-3">{product.name}</h3>
+                    <span>Size: {product.size}</span>
                   </div>
-                  <h3 className="mb-3">{product.name}</h3>
-                  <span>Size: {product.size}</span>
-                </div>
-                <div className="position-relative mt-auto">
-                  <img
-                    className="img-fluid"
-                    src={`/static/img/${product.images.split(",")[0]}`} // Use the first image for the product card
-                    alt={product.name}
-                  />
-                  <div className="product-overlay">
-                    <button
-                      className="btn btn-lg-square btn-outline-light rounded-circle"
-                      onClick={() => openModal(product)}
-                    >
-                      <i className="fa fa-eye text-primary"></i>
-                    </button>
+                  <div className="position-relative mt-auto">
+                    <img
+                      className="img-fluid"
+                      src={`/static/img/${product.images.split(",")[0]}`} // Use the first image for the product card
+                      alt={product.name}
+                    />
+                    <div className="product-overlay">
+                      <button
+                        className="btn btn-lg-square btn-outline-light rounded-circle"
+                        onClick={() => openModal(product)}
+                      >
+                        <i className="fa fa-eye text-primary"></i>
+                      </button>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
+        </div>
+
+        {/* Section for Cupcakes */}
+        <div className="mb-5">
+          <h2 className="text-primary mb-4">Cupcakes</h2>
+          <div className="row g-4">
+            {filterByCategory("cupcakes").map((product) => (
+              <div className="col-lg-4 col-md-6" key={product._id}>
+                <div className="product-item d-flex flex-column bg-white rounded overflow-hidden h-100">
+                  <div className="text-center p-4">
+                    <div className="d-inline-block border border-primary rounded-pill px-3 mb-3">
+                      {product.price}
+                    </div>
+                    <h3 className="mb-3">{product.name}</h3>
+                    <span>Size: {product.size}</span>
+                  </div>
+                  <div className="position-relative mt-auto">
+                    <img
+                      className="img-fluid"
+                      src={`/static/img/${product.images.split(",")[0]}`} // Use the first image for the product card
+                      alt={product.name}
+                    />
+                    <div className="product-overlay">
+                      <button
+                        className="btn btn-lg-square btn-outline-light rounded-circle"
+                        onClick={() => openModal(product)}
+                      >
+                        <i className="fa fa-eye text-primary"></i>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {/* Section for Cookies */}
+        <div className="mb-5">
+          <h2 className="text-primary mb-4">Cookies</h2>
+          <div className="row g-4">
+            {filterByCategory("cookies").map((product) => (
+              <div className="col-lg-4 col-md-6" key={product._id}>
+                <div className="product-item d-flex flex-column bg-white rounded overflow-hidden h-100">
+                  <div className="text-center p-4">
+                    <div className="d-inline-block border border-primary rounded-pill px-3 mb-3">
+                      {product.price}
+                    </div>
+                    <h3 className="mb-3">{product.name}</h3>
+                    <span>Size: {product.size}</span>
+                  </div>
+                  <div className="position-relative mt-auto">
+                    <img
+                      className="img-fluid"
+                      src={`/static/img/${product.images.split(",")[0]}`} // Use the first image for the product card
+                      alt={product.name}
+                    />
+                    <div className="product-overlay">
+                      <button
+                        className="btn btn-lg-square btn-outline-light rounded-circle"
+                        onClick={() => openModal(product)}
+                      >
+                        <i className="fa fa-eye text-primary"></i>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
 
         {modalProduct && (
@@ -106,12 +185,4 @@ const Product = () => {
   );
 };
 
-console.log("DOM fully loaded");
-
-const productRoot = document.getElementById("product");
-if (productRoot) {
-  console.log("Found #product, rendering React component...");
-  ReactDOM.createRoot(productRoot).render(<Product />);
-} else {
-  console.error("No #product div found in the DOM.");
-}
+export default Product;
